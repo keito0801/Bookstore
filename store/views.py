13056a80e0ffcc6bdf2791634cd4baefe4a11a44
Keith Gonzalez <kg0801@yahoo.com>
2021-05-22 23:26:33 -0400

    finished form

--- store/views.py
+++ store/views.py
@@ -3,11 +3,45 @@ from .models import Book
 
 # Create your views here.
 def index(request):
-    return render(request, 'store/home.html')
+    return render(request, 'store/home.html', {
+        'page_name': 'index'
+    })
 
 def store(request):
-    book_count = Book.objects.all().count()
+    books = Book.objects.all()
+    book_count = books.count()
     context = {
-        'count': book_count
+        'books': books,
+        'count': book_count,
+        'page_name': 'store',
     }
     return render(request, 'store/store.html', context)
+
+def new_book(request):
+    context = {
+        'page_name': 'new_book'
+    }
+    return render(request, 'store/new_book.html')
+
+def new_book(request):
+    if request.method == "POST":
+        print(request.POST)
+        new_book = Book.objects.create(
+            title=request.POST.get('title'),
+            author=request.POST.get('author'),
+            description=request.POST.get('description'),
+            publish_date=request.POST.get('publish_date')
+        )
+        new_book.save()
+        context = {
+            'page_name': 'new_book',
+            'show_form': False
+        }
+        return render(request, 'store/new_book.html', context)
+    else: # elif request.method == "GET":
+        context = {
+        'page_heading': 'Create a Book',
+        'page_name': 'new_book',
+        'show_form': True
+    }    
+    return render(request, 'store/new_book.html', context)
